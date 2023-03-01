268aa240f651a397e4d5a0dccb63b2ea2d5f2182
dib-676 <126702853+dib-676@users.noreply.github.com>
2023-03-01 22:28:19 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View,StyleSheet} from 'react-native'
+import TodoList from './TodoList/List/index'
+
+class App extends React.Component{
+  render(){
+    return(
+        <TodoList/>
+    )
+  }
+}
+export default App
